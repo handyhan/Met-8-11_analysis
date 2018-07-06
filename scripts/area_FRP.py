474d7f139ca67d83bf2e65ccf300c0060c5983e5
Hannah N <hannah.nguyen@kcl.ac.uk>
2018-07-06 15:38:25 +0100

    code to rergid VZA to 1 degree over Africa and europe

--- scripts/area_FRP.py
+++ scripts/area_FRP.py
@@ -19,6 +19,7 @@ from math import sin, cos, sqrt, atan2, radians
 from scipy import stats
 from datetime import datetime
 from netCDF4 import Dataset
+from time import gmtime, strftime
 
 
 def read_in_from_csv(dates):
@@ -32,6 +33,8 @@ def read_in_from_csv(dates):
             m8_pixels = pd.concat([m8_pixels,m8_pixels_day],axis=0, join='outer')
             m11_pixels = pd.concat([m11_pixels,m11_pixels_day],axis=0, join='outer')
         """
+        
+        print 
         return m8_pixels, m11_pixels
     else:
         print "Requested dates do not exist"
@@ -72,24 +75,11 @@ def proccess_grid(df,lon,lat, sat,resolution):
     grid = pd.DataFrame({'GRID_NO':fire_ids, 'summed_FRP': frp_sum.values[:],'FRP_uncertainty':frp_err.values[:],'mean_vza': mean_vza.values[:], 'pixel_count':pixel_count , 'SAT': sat,'LAT_MIN':lat_min,'LAT_MAX':lat_max,'LAT_CENTER':lat_center, 'LONG_MIN':long_min,'LONG_MAX':long_max,'LONG_CENTER':long_center, })
     return grid, fire_ids
 
-        
-
-def eliminate_zero_grid(m8_grid,m11_grid):
-    
-    m8_grid_list = np.unique((m8_grid[m8_grid['GRID_NO'] >= 0 ])['GRID_NO']) 
-    m11_grid_list = np.unique((m11_grid[m11_grid['GRID_NO'] >= 0 ])['GRID_NO']) 
-    grid_list = np.unique(np.concatenate((m11_grid_list,m8_grid_list)))
-    
-    m8_grid = m8_grid.loc[grid_list]
-    
-    m11_grid = m11_grid.loc[grid_list]
-    return m8_grid, m11_grid
-    
 
 
-def write_gridded_file(m8_df,m11_df):
+def write_gridded_file(lonmin,lonmax,latmin,latmax,m8_df,m11_df):
 
-    
+    """
     m8_df = m8_df[(m8_df['LATITUDE'] > latmin) & (m8_df['LATITUDE'] < latmax) ]
     m8_df = m8_df[(m8_df['LONGITUDE'] > lonmin) & (m8_df['LONGITUDE'] < lonmax) ]
     m8_df = m8_df.reset_index(drop=True)
@@ -97,95 +87,173 @@ def write_gridded_file(m8_df,m11_df):
     m11_df = m11_df[(m11_df['LONGITUDE'] > lonmin) & (m11_df['LONGITUDE'] < lonmax) ]
     m11_df = m11_df.reset_index(drop=True)
     
+    
     m8_grid,grid_boxes_m8 = proccess_grid(m8_df,lon,lat,'8',resolution)
     m11_grid,grid_boxes_m11 = proccess_grid(m11_df,lon,lat,'11',resolution) 
      
     joint_boxes =  np.concatenate([grid_boxes_m8,grid_boxes_m11])
     joint_boxes = pd.Series(data=joint_boxes )
-    joint_boxes = joint_boxes[joint_boxes.duplicated()]
+    joint_boxes = joint_boxes[joint_boxes.duplicated()] 
     m8_grid = m8_grid[m8_grid['GRID_NO'].isin(joint_boxes.values[:])]
     m11_grid = m11_grid[m11_grid['GRID_NO'].isin(joint_boxes.values[:])]
+    
+    
+    grid_df = pd.DataFrame(joint_boxes.str.split(' ',1).tolist(), columns = ['lat_box','long_box'])
+    grid_df.lat_box  = (grid_df.lat_box.apply(lambda x: x.lstrip('0')).astype(int))-1
+    grid_df.long_box  = (grid_df.long_box.apply(lambda x: x.lstrip('0')).astype(int))-1
+    
     """
-    m8_grid['M8_summed_FRP'] = m8_grid['summed_FRP'] 
-    m8_grid['M8_FRP_uncertainty'] = m8_grid['FRP_uncertainty'] 
-    m8_grid['M8_PIXEL_COUNT'] = m8_grid['pixel_count'].astype(int)
     
-    m8_grid['M11_summed_FRP'] = m11_grid['summed_FRP'] 
-    m8_grid['M11_FRP_uncertainty'] = m11_grid['FRP_uncertainty'] 
-    m8_grid['M11_PIXEL_COUNT'] = m11_grid['pixel_count'].astype(int)
-    m8_grid['FRP_diff'] = m8_grid['M11_summed_FRP'] - m8_grid['M8_summed_FRP']
+    regrid_vza(lonmin,lonmax,latmin,latmax)
     
-    grid = m8_grid
-    grid = grid.drop(columns=['summed_FRP','pixel_count','mean_vza','SAT','FRP_uncertainty'])
+    
+    
+    #grid = pd.concat([m8_grid,m11_grid])
+    
+    #return grid
+    
+    
+    
+def regrid_vza(lonmin,lonmax,latmin,latmax):
+    
+    
+    lon = np.arange(lonmin,(lonmax+resolution),resolution)
+    lat = np.arange(latmin,(latmax+resolution),resolution)
+    
+    deg_1_df = pd.DataFrame({'long':lon,'lat':lat})
+    deg_1_df['central_lat'] = deg_1_df['lat'] + 0.5 
+    deg_1_df['central_long'] = deg_1_df['long'] + 0.5 
+    
+
+    BaseDir = "C:/Users/Hannah.N/Documents/Earth_Observation/sandbox"
+    m8_vza = BaseDir +"/SEVIRI_Static/HDF5_LSASAF_MSG_VZA_IODC-Disk_201807011030" 
+    m11_vza = BaseDir +"/SEVIRI_Static/HDF5_LSASAF_MSG_VZA_IODC-Disk_201807011030"
+    m11_lat = BaseDir +"/SEVIRI_Static/HDF5_LSASAF_MSG_LAT_IODC-Disk_201711300000"
+    m11_long = BaseDir +"/SEVIRI_Static/HDF5_LSASAF_MSG_LON_IODC-Disk_201711300000"
+    
+    m8_vza = h5py.File(m8_vza,'r+')
+    m11_vza = h5py.File(m11_vza,'r+') 
+    
+    print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    #m11_lat_lon = pd.read_csv(BaseDir + "/txt_data/MSG_lat_lon_3000")
+    
+    msg_rows = [0,500,1000,1500,2000,2500,3000]
+    m11_lat_lon = pd.read_csv(BaseDir + "/txt_data/MSG_lat_lon_"+str(msg_rows[0]))
+    for file in msg_rows[1:]:
+        m11_lat_lon_n=pd.read_csv(BaseDir + "/txt_data/MSG_lat_lon_"+str(file))
+        m11_lat_lon =  m11_lat_lon.append(m11_lat_lon_n,ignore_index=True)
+    
+    
+    m11_lat_lon = m11_lat_lon[(m11_lat_lon['LONGITUDE'] >= (lonmin*100)) & (m11_lat_lon['LONGITUDE'] <= (lonmax*100))]
+    m11_lat_lon = m11_lat_lon[(m11_lat_lon['LATITUDE'] >= (latmin*100)) & (m11_lat_lon['LATITUDE'] <= (latmax*100))]
+    #print m11_lat_lon
+    #m11_lat_lon = m11_lat_lon.sort_values(['I_val'])
+    #m11_lat_lon.reset_index(drop=True,inplace=True)
+    #m11_lat_lon = m11_lat_lon.iloc[::10]
+    m11_lat_lon = m11_lat_lon.sample(len(m11_lat_lon['LONGITUDE'])/50)
+    #m11_lat_lon = m11_lat_lon.sort_values(['J_val'])
+    #m11_lat_lon.reset_index(drop=True,inplace=True)
+    #m11_lat_lon = m11_lat_lon.iloc[::20]
+    #print m11_lat_lon
+    
+    
+    m11_lat_lon.reset_index(drop=True,inplace=True)
+    
+    m11_lat_lon['LONGITUDE'] =m11_lat_lon['LONGITUDE']/100
+    m11_lat_lon['LATITUDE'] =m11_lat_lon['LATITUDE']/100
+    
+    
+    m11_lat_lon['lon_index'] = pd.cut(m11_lat_lon['LONGITUDE'],lon,labels=False,include_lowest=True).values[:]
+    m11_lat_lon['lat_index'] = pd.cut(m11_lat_lon['LATITUDE'],lat,labels=False,include_lowest=True).values[:]
+    
+    
+    #m11_lat_lon = m11_lat_lon[m11_lat_lon['lon_index'].isin(grid_df['long_box'].values[:])]
+    #m11_lat_lon = m11_lat_lon[m11_lat_lon['lat_index'].isin(grid_df['lat_box'].values[:])]
+    
+    #m11_lat_lon = m11_lat_lon[(m11_lat_lon['lat_index'] < 5) ]#& (m11_lat_lon['lon_index'] < 2)]
+    m11_lat_lon.reset_index(drop=True,inplace=True)
+    
+    m11_lat_lon['VZA'] = m11_lat_lon.index    
+    m11_lat_lon['VZA'] = (m11_lat_lon.VZA.apply(lambda x: m11_vza['VZA'][m11_lat_lon['J_val'][x]][m11_lat_lon['I_val'][x]]))/100
+    #print m11_lat_lon
+    
+    
+    m11_lat_lon = m11_lat_lon.sort_values(by=['lon_index','lat_index'],ascending=True)
+    
+    m11_lat_lon['bin_id'] =  ((m11_lat_lon['lat_index']*1000)+m11_lat_lon['lon_index']).astype(int)
+    
+    
+    #vza_grid = pd.DataFrame({'lon_index':lon_grid_cells,'lat_index':lat_grid_cells})
+    
+    #vza_grid['GRID_NO'] = 
+     
+    vza_mean = m11_lat_lon.groupby(['bin_id'])['VZA'].mean()
+    
+    
+    
+    vza_grid = pd.DataFrame({'bin_id':np.unique(m11_lat_lon['bin_id'])})
+    vza_grid['VZA'] = vza_mean.values[:]
+    vza_grid['lat_index'] = (vza_grid['bin_id'].astype(np.float)/1000).astype(int)
+    vza_grid['lon_index'] = vza_grid['bin_id'] - (vza_grid['lat_index']*1000)
+    
+    print vza_grid
+    
+    vza_grid['LATITUDE'] =  vza_grid.lat_index.apply(lambda x: deg_1_df['central_lat'].iloc[x])
+    vza_grid['LONGITUDE'] =  vza_grid.lon_index.apply(lambda x: deg_1_df['central_long'].iloc[x])
+    print vza_grid
+    
+    
+    
+    vza_grid.to_csv("C:/Users/Hannah.N/Documents/Earth_Observation/sandbox/txt_data/MET_11_1Deg_VZA",header=True,index=True)
+    vza_grid.plot(x='LONGITUDE',y='LATITUDE',c = 'VZA', kind='scatter')
+    
+    
+    
+    
+    
+    
+    
+    print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    #print m11_lat_lon['VZA'].mean()
+    
+    #m11_lat_lon.plot(x='LATITUDE',y='LONGITUDE',kind='scatter')
+    #print m11_df[m11_df['LATITUDE']]
+    
+    
+    #print m11_vza['VZA'][:][:]
+    
+    
+    
     """
-    grid = pd.concat([m8_grid,m11_grid])
-    return grid
-    
+    for i in range(0,len(m8_df['PIXEL_ROW'] -1),1):
+            pix_col = int(m8_df['PIXEL_COL'][i]) -1
+            pix_row = int(m8_df['PIXEL_ROW'][i] - 1)
+            Q_flag =  m8_QP['QUALITYFLAG'][pix_row][pix_col]
+            if Q_flag == 2 :
+                drop_list.append(i)
+        m8_df = m8_df.drop(m8_df.index[drop_list])        
+        m8_df = m8_df.reset_index(drop=True)
+        drop_list=[]
+        for i in range(0,len(m11_df['PIXEL_ROW'] -1),1):
+            pix_col = int(m11_df['PIXEL_COL'][i]) -1
+            pix_row = int(m11_df['PIXEL_ROW'][i]) - 1
+            Q_flag =  m11_QP['QUALITYFLAG'][pix_row][pix_col]
+            if Q_flag == 2 :
+                drop_list.append(i)
+        m11_df = m11_df.drop(m11_df.index[drop_list])        
+        m11_df = m11_df.reset_index(drop=True)
+     """ 
 
-
-def fit_and_plot_OLS_bins(fire_data):  
-
-    results, outlier_ix ,slope_all, intercept_all, r2_all, std_err_all, residual = regression_free(fire_data, 'M11_summed_FRP','M8_summed_FRP') 
-    print results, outlier_ix ,slope_all, intercept_all, r2_all, std_err_all, residual
-    ####remove outliers and re-fit
-    #fire_data = fire_data[~fire_data.iloc[:].index.isin(outlier_ix)]
-    #results, outlier_ix ,slope_all, intercept_all, r2_all, std_err_all, residual = regression_free(fire_data, 'M11_summed_FRP','M8_summed_FRP')
-    #outlier_count_all = len(outlier_ix)
-    #outlier_fires = fire_data[fire_data.iloc[:].index.isin(outlier_ix)]
-    #plot_bin_maps(outlier_fires,fires_m8,'clusters')
-    #######
-    
-    fire_max = max(fire_data['M11_summed_FRP'].max(),fire_data['M8_summed_FRP'].max())
-    bias_all = fire_data['FRP_diff'].mean()
-    SD_all = fire_data['FRP_diff'].std()
-    rmsd_all = sqrt(bias_all**2 + SD_all**2)
-    fire_count_all = len(fire_data['M11_summed_FRP'])
-    
-    fit_points = (np.array([0,max(fire_data['M11_summed_FRP'].values)]))
-    perf_fit = (fit_points)*1 + 0
-    z = (fit_points)*slope_all + intercept_all
-    textstr1 = 'y = %.2fx + %.2f \n Fire count=%d \n R$^{2}$=%.3f\n SE=%.3f'%(slope_all, intercept_all,fire_count_all, r2_all, std_err_all)
-    textstr2 = 'Bias=%.3f \n  Scatter =%.3f \n RMSD=%.3f'%(bias_all, SD_all, rmsd_all)
-    plot_scatter_fit(fire_data['M11_summed_FRP'],fire_data['M8_summed_FRP'],z,perf_fit,fit_points,'all',('./Plots/FRP_ols'+plot_tag), textstr1,textstr2,fire_max)
-    #plot_scatter_residuals(fire_data['M11_summed_FRP'],fire_data['M8_summed_FRP'],z, residual,slope_all,intercept_all,vza_max,fire_count_all,'all',('./Plots/FRP_OLS'+plot_tag))
-    stats_f = pd.DataFrame({'VZA_BIN':vza_bins,'BIAS':bias_all,'SCATTER':SD_all,'RMSD':rmsd_all,'FIRE_COUNT':fire_count_all,'SLOPE':slope_all,'INTERCEPT':intercept_all, 'R2':r2_all,'SE':std_err_all})
-    
-
-    return stats_f
-       
-    
-
-def fit_and_plot_ODR_bins(grid):
-    print grid['M11_summed_FRP'],grid['M8_summed_FRP'],grid['M11_FRP_uncertainty'],grid['M8_FRP_uncertainty']
-    
-    params,param_uncertainty, adjusted_err, residual = odr_regression(grid['M11_summed_FRP'],grid['M8_summed_FRP'],grid['M11_FRP_uncertainty'],grid['M8_FRP_uncertainty'])
-    print params,param_uncertainty, adjusted_err, residual
-    fire_max = max(grid['M11_summed_FRP'].max(),grid['M8_summed_FRP'].max())
-    bias_all = grid['FRP_diff'].mean()
-    SD_all = grid['FRP_diff'].std()
-    rmsd_all = sqrt(bias_all**2 + SD_all**2)
-    fire_count_all = len(grid['M8_summed_FRP'])
-    
-    fit_points = (np.array([0,max(grid['M11_summed_FRP'].values)]))
-    m8_fit = fit_points*params[1] + params[0]
-    perf_fit = fit_points*1 + 0
-    
-    plot_ODR_scatter_residuals(grid['M11_summed_FRP'],grid['M8_summed_FRP'],grid['M11_FRP_uncertainty'],grid['M8_FRP_uncertainty'],m8_fit,perf_fit,fit_points, residual,adjusted_err,params, fire_max,fire_count_all,'all',('C:/Users/Hannah.N/Documents/Earth_Observation/sandbox/Plots/AREA_FRP_ODR'+plot_tag))
-        
-    stats_f = pd.DataFrame({'VZA_BIN':vza_bins,'BIAS':bias_all,'SCATTER':SD_all,'RMSD':rmsd_all,'FIRE_COUNT': fire_count_all,'SLOPE':params[1] ,'INTERCEPT':params[0],'SE_slope':param_uncertainty[1],'SE_intercept':param_uncertainty[0]})
-
-    return stats_f
-
-
-#### Parameters ####
     
 BaseDir = "C:/Users/Hannah.N/Documents/Earth_Observation/sandbox"
 
+
+
 resolution = 1.0
-lonmin = 10.0
-lonmax = 30.0
-latmin = -10.0
-latmax = 10.0
+lonmin = -20.0
+lonmax = 60.0
+latmin = -35.0
+latmax = 45.0
 lon = np.arange(lonmin,(lonmax+resolution),resolution)
 lat = np.arange(latmin,(latmax+resolution),resolution)
 steps_lat = len(lat)
@@ -200,11 +268,13 @@ lat_max = np.repeat(lat[1:],steps_lat)
 
     #m8_grid = pd.DataFrame({'MIN_LAT':lat_min,'MAX_LAT':lat_max,'MIN_LONG':lon_min,'MAX_LONG':lon_max,'GRID_NO': None,'summed_FRP': 0,'FRP_uncertainty':0,'mean_vza': 0, 'pixel_count':0 , 'SAT': 8})
 
-dates = pd.date_range(start='2018-06-09',end='2018-07-03')
+
+
+dates = pd.date_range(start='2018-06-10',end='2018-06-10')
 dates = dates.format(formatter=lambda x: x.strftime('%Y%m%d'))
 for date in dates:
     print "NOTE: Generating fires for "  + date
-    times = pd.date_range(start='00:00',end='23:45', freq='15min')
+    times = pd.date_range(start='12:00',end='12:00', freq='15min')
     times=times.format(formatter=lambda x: x.strftime('%H%M'))
     header_1=True
     for time in times:
@@ -215,12 +285,15 @@ for date in dates:
             print "ERROR while opening file"
             
         else:    
-            grid = write_gridded_file(m8_df,m11_df)
-            grid['DATE'] = date
-            grid['TIME'] = time
-            mode = 'w' if header_1 else 'a'
-            grid.to_csv(BaseDir +"/txt_data/AREA_grid_"+date, mode=mode,header=header_1,index=False) 
-            header_1=False
+            #grid = 
+            write_gridded_file(lonmin,lonmax,latmin,latmax,m8_df,m11_df)
+            #grid['DATE'] = date
+            #grid['TIME'] = time
+            
+            
+            #mode = 'w' if header_1 else 'a'
+            #grid.to_csv(BaseDir +"/txt_data/AREA_grid_"+date, mode=mode,header=header_1,index=False) 
+            #header_1=False
             
             
 #print grid 
